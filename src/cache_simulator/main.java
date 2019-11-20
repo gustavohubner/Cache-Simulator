c4616bce3e9699e963347c2227a12c781eb8f45c
Gustavo Rehbein <gustavohubner98@yahoo.com.br>
2019-11-20 18:33:57 -0300

    arg

--- src/cache_simulator/main.java
+++ src/cache_simulator/main.java
@@ -1,21 +1,29 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package cache_simulator;
 
-/**
- *
- * @author gusta
- */
 public class main {
 
     /**
-     * @param args the command line arguments
+     * @param args the command line arguments nsets: número de conjuntos na
+     * cache;
+     *
+     * bsize: tamanho do bloco em bytes;
+     *
+     * assoc: grau de associatividade;
+     *
+     * substituição: política de substituição, que pode ser Random (R), FIFO (F)
+     * ou L (LRU);
+     *
+     * flag_saida: flag que ativa o modo padrão de saída de dados;
+     *
+     * arquivo_de_entrada: arquivo com os endereços para acesso à cache.
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int nsets, bsize, assoc, flag_saida;
+        char subst, flag;
+
+        for (String arg : args) {
+            System.out.print(arg + " ");
+        }
     }
-    
+
 }
